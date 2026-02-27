6e890df518099c2ccbec0262416c2e89b91730ad
Izuku <zklq1e@gmail.com>
2026-02-28 00:17:03 +0530

    convert backend to serverless

---
+++ server/index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+module.exports = app;
--- server/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-    console.log(`[VISTA] Server running on port ${PORT}`);
-});
